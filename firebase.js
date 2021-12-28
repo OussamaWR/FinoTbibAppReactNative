d44a3c8f7b42d22d07b4b2e056f6029b41238532
oussama <oussamawr6@gmail.com>
2021-12-28 13:05:48 +0100

    commit

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@
 import * as firebase from "firebase/app";
 
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// https://firebase.google.com/docs/web/setup#available-libraries 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
