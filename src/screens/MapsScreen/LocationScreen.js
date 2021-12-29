70880e654489cdbfe5c0847d57b4ec1c984fae27
oussama <oussamawr6@gmail.com>
2021-12-29 11:38:29 +0100

    oussama commit

--- src/screens/MapsScreen/LocationScreen.js
+++ src/screens/MapsScreen/LocationScreen.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { StyleSheet, Text, View,Image } from 'react-native'
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
+import { LocalTile } from 'react-native-maps';
 import MapView ,{PROVIDER_GOOGLE,Marker,Callout,Circle} from 'react-native-maps'
 
 
@@ -32,23 +33,43 @@ const LocationScreen = () => {
             <MapView
             provider={PROVIDER_GOOGLE}
             style={{height:"58%" }}
-
-                initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}
+                showsUserLocation={true}
+                followsUserLocation={true}
+                rotateEnabled={true}
+                zoomEnabled={true}
+                toolbarEnabled={true}
+                // initialRegion={{
+                // latitude: 37.78825,
+                // longitude: -122.4324,
+                // latitudeDelta: 0.0922,
+                // longitudeDelta: 0.0421,
+                // }}
               >
+              <LocalTile
+                    /**
+                        * The path template of the locally stored tiles. The patterns {x} {y} {z} will be replaced at runtime
+                        * For example, /storage/emulated/0/mytiles/{z}/{x}/{y}.png
+                        */
+                    pathTemplate={this.state.pathTemplate}
+                    /**
+                        * The size of provided local tiles (usually 256 or 512).
+                        */
+                    tileSize={256}
+                    />
 
                
                     <Marker
                 coordinate={{latitude:37.8025259, longitude:-122.4351431}}
+                // image={{uri:"https://picsum.photos/200"}}
+                // style={{height:10,width:10}}
                 >
-                    <Callout>  
+                    <Callout 
+                     
+                      style={{height:100,width:100}}
+                    >  
                     
                    {/* <Text style={{ height: 100, position: "relative", textAlign:'center' , backgroundColor:'gray',marginTop:-25}}> */}
-                    <Image
+                    {/* <Image
                      resizeMode= 'contain'
                       source={ require("../../../assets/images/tabib.jpg")}
                       style={{
@@ -57,7 +78,7 @@ const LocationScreen = () => {
                         // resizeMode:'contain'
                         
                       }}
-                    />
+                    /> */}
                     {/* </Text> */}
                     <Text>
                         tbib l9alb
