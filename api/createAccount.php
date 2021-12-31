0b4a07fc9fc127e064ff68f22d622ec7303d20ca
Mouad Sellak <mouaadsellak123@gmail.com>
2021-12-31 10:47:43 +0100

    mouad modify api

--- api/createAccount.php
+++ api/createAccount.php
@@ -1,22 +1,18 @@
 <?php
 require_once("config.php");
-
-    $fullname = $DecodedData['fullname'];
-    $login = $DecodedData['username'];
-    $pwd = $DecodedData['password'];
-
-    // $fullname = $_POST['fullname'];
-    // $email = $_POST['email'];
-    // $login = $_POST['username'];
-    // $pwd = $_POST['password'];
-
-    try{
-        $Requete = "INSERT into users(fullname,username,password) VALUES('$fullname','$login',MD5('$pwd') ) ";
-        $Resultat = $pdo->query($Requete);
-        $Message='Record inserted succesfully !' ;
-    } catch(Exception $e){
-        $Message=$e->getMessage();
-    }
-    $Response=array('Message' => $Message);
-    echo json_encode($Response); 
-
+$fullname = $DecodedData['fullname'];
+$login = $DecodedData['username'];
+$pwd = $DecodedData['password'];
+$Requete = "INSERT into users(fullname,username,password) VALUES('$fullname','$login',MD5('$pwd') ) ";
+$Resultat = $pdo->query($Requete);
+if ($Resultat) {
+    $Message = 'Account created successfully !';
+    echo $Message;
+} else {
+    $Message = 'Error Something went wrong !';
+    echo $Message;
+}
+// $fullname = $_POST['fullname'];
+// $email = $_POST['email'];
+// $login = $_POST['username'];
+// $pwd = $_POST['password'];
